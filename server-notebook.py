e73002d87df1836c6e872e636c236173a2d42646
michael-conway <michael_conway@unc.edu>
2016-12-01 11:43:50 -0500

    added server-notebook.py skeleton

--- server-notebook.py
+++ server-notebook.py
@@ -7,10 +7,20 @@ import random
 from datetime import datetime
 
 
+# Define our help request resource.
+class MasterRequest(Resource):
+    def get(self):
+        response = make_response(
+            render_template(
+                'nb-master.html'), 200)
+        response.headers['Content-Type'] = "text/html"
+        return response
 
 # Assign URL paths to our resources.
 app = Flask(__name__)
 api = Api(app)
+api.add_resource(MasterRequest, '/master')
+
 
 # Start the server.
 if __name__ == '__main__':
